8a1ded3097469055a52ec30f52ce5dd8c7825121
ClaireV38 <claireverdier00@gmail.com>
2021-05-12 15:24:04 +0200

    get token from laravel back after login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class SecurityController extends AbstractController
 {
@@ -32,5 +35,43 @@ class SecurityController extends AbstractController
     public function logout()
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    /**
+     * @Route("/token", name="app_token")
+     * @param HttpClientInterface $client
+     * @param EntityManager $entityManager
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     * @throws \Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
+     */
+    public function getToken(HttpClientInterface $client, EntityManagerInterface $entityManager)
+    {
+        $arrayAuthenticate = [
+            'grant_type' => 'password',
+            'client_id' => '93696e7e-e302-4ebe-b50f-e976e7a20f2a',
+            'client_secret' => 'JVnptbFa3HrCygv1b7Lz9MqapfNGvR83DabSMDQM',
+            'username' => 'su@gmail.com',
+            'password' => '1234',
+        ];
+        $response = $client->request(
+            'POST',
+            'http://127.0.0.1:8000/oauth/token', [
+                'headers' => [
+                    'Accept' => 'application/json',
+                ],
+                'json' =>
+                    $arrayAuthenticate
+            ]
+        );
+        $user = $this->getUser();
+        $user->setApiToken($response->toArray()['access_token']);
+        $entityManager->flush();
+        return $this->redirectToRoute('app_index');
     }
 }
