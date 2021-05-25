f0dfd70db95906d75db5f6a5b7549b97712ce10a
ClaireV38 <claireverdier00@gmail.com>
2021-05-25 18:39:55 +0200

    display all treasures for one user

---
+++ src/Controller/AdventurerController.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class AdventurerController extends AbstractController
+{
+    /**
+     * @Route("adventurer/", name="adventurer_index")
+     * @param HttpClientInterface $client
+     * @return Response
+     * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
+     */
+    public function index(HttpClientInterface $client): Response
+    {
+        $adventurer = $this->getUser();
+
+        $response = $client->request(
+            'GET',
+            'http://127.0.0.1:8000/api/v1/adventurers/' . $adventurer->getId()
+        );
+        $adventurerAssets = $response->toArray()['data'];
+
+        return $this->render('adventurer/index.html.twig', [
+            'assets' => $adventurerAssets
+        ]);
+
+    }
+}
