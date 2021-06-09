2fb427f58fc5f8b1e25e0bc5023a1f41e6adefe6
ClaireV38 <claireverdier00@gmail.com>
2021-06-09 16:56:18 +0200

    allow update asset from front app

--- src/Controller/AssetController.php
+++ src/Controller/AssetController.php
@@ -13,6 +13,11 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
 
 /**
  * @Route("/asset", name="asset_")
@@ -117,19 +122,63 @@ class AssetController extends AbstractController
      * @param int $id
      * @return Response
      */
-    public function edit(Request $request, int $id): Response
+    public function edit(HttpClientInterface $client, Request $request, int $id): Response
     {
+        $encoders = [new XmlEncoder(), new JsonEncoder()];
+        $normalizers = [new ObjectNormalizer()];
+
+        $serializer = new Serializer($normalizers, $encoders);
+
+        $token = $this->getUser()->getApiToken();
+
+        $response = $client->request(
+            'GET',
+            'http://127.0.0.1:8000/api/v1/assets/' . $id, [
+            'headers' => [
+                'Accept' => 'application/json',
+                'Authorization' => 'Bearer ' . $token
+            ]]);
+        $objectResponse = json_decode($response->getContent());
+
+        $asset = $objectResponse->data[0];
+
         $assetData = new AssetData();
+        $assetData->setTitle($asset->title);
+        $assetData->setPlaceOfDiscovery($asset->place_of_discovery);
+        $assetData->setValue($asset->value);
+        $assetData->setPhoto($asset->photo);
+
         $form = $this->createForm(AssetType::class, $assetData);
         $form->handleRequest($request);
 
+
         if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
+            $arrayAsset = (array)$assetData;
+
+            $stringParams = "";
+            foreach ($arrayAsset as $key => $parameter) {
+                if ($key === array_key_last($arrayAsset)) {
+                    $stringParams .= $key . "=" . $parameter;
+                } else {
+                    $stringParams .= $key . "=" . $parameter . "&";
+                }
+            }
+
+            $response = $client->request(
+                'PUT',
+                'http://127.0.0.1:8000/api/v1/assets/' . $id . '?' . $stringParams , [
+                    'headers' => [
+                        'Accept' => 'application/json',
+                        'Authorization' => 'Bearer ' . $token,
+                    ]
+                ]
+            );
 
             return $this->redirectToRoute('adventurer_index');
         }
 
         return $this->render('asset/edit.html.twig', [
+            'asset' => $asset,
             'form' => $form->createView(),
         ]);
     }
